7f417128b1987b0901a8fceea741c02027f0d9c8
root <root@localhost.localdomain>
2018-09-22 11:32:41 -0400

    segundo commit

--- base/patch00000001.sql
+++ base/patch00000001.sql
@@ -7,3 +7,8 @@ CREATE TABLE hd.ttipo_requerimiento (
 ) INHERITS (pxp.tbase)
 WITHOUT OIDS;
 /*****************************F-SCP-JRR-HD-0-22/09/2018*************/
+
+/*****************************I-SCP-JRR-HD-1-22/09/2018*************/
+alter table hd.ttipo_requerimiento
+add column cantidad integer;
+/*****************************F-SCP-JRR-HD-1-22/09/2018*************/
