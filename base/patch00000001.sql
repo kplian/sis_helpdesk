8ff8726be20c7510dceec600dc19e9ff76a5a161
KPLIAN <kplian@kplian.com>
2018-10-05 23:53:12 -0400

    Update patch00000001.sql

--- base/patch00000001.sql
+++ base/patch00000001.sql
@@ -61,33 +61,5 @@ ALTER TABLE hd.trequerimiento
 
 COMMENT ON COLUMN hd.trequerimiento.id_tipo_requerimiento
 IS 'IDENTIFICADOR FORANEO DE LA TABLA DONDE ESTAN LOS TIPOS DE REQUERIMIENTOS';
-
-ALTER TABLE hd.trequerimiento
-  ADD CONSTRAINT trequerimiento_id_requerimiento_fk FOREIGN KEY (id_requerimiento_anterior)
-    REFERENCES hd.trequerimiento(id_requerimiento)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    NOT DEFERRABLE;
-    
-ALTER TABLE hd.trequerimiento
-  ADD CONSTRAINT trequerimiento_id_institucion_externa_fk FOREIGN KEY (id_institucion_externa)
-    REFERENCES param.tinstitucion(id_institucion)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    NOT DEFERRABLE;
-    
-ALTER TABLE hd.trequerimiento
-  ADD CONSTRAINT trequerimiento_id_usuario_fk FOREIGN KEY (id_usuario)
-    REFERENCES segu.tusuario(id_usuario)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    NOT DEFERRABLE;
-    
-ALTER TABLE hd.trequerimiento
-  ADD CONSTRAINT trequerimiento_id_tipo_requerimiento_fk FOREIGN KEY (id_tipo_requerimiento)
-    REFERENCES hd.ttipo_requerimiento(id_tipo_requerimiento)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    NOT DEFERRABLE;
 /*****************************F-SCP-JMH-HD-0-05/10/2018*************/
 
